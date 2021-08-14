53e4584dc223ae843debc064504f88dd0ae80560
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-14 18:27:10 +0530

    feat: Welcome Message

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StockAccountManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome to Stock Account Management!");
+        }
+    }
+}
